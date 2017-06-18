8ca40f32506147898571c5d5054df48997e9233e
Ricardo Nazario <nazarioricardo@gmail.com>
2017-06-18 12:44:55 -0400

    Responsive

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+
     $('#video-background').prop('volume', 0)
     let isPlaying = false
     let navFixed = false
@@ -7,69 +8,68 @@ $(document).ready(function() {
         return $.trim($(this).text())
     }).get()
 
-    console.log(list)
+    // console.log(list)
 
-    $('#video').click(function() {
-        if (isPlaying) {
-            $('#video-background').animate({volume: 0})
-            $('.hide').fadeIn('slow')
-            isPlaying = false
-        } else {
-            $('#video-background').animate({volume: 1})
-            $('.hide').fadeOut('slow')
-            isPlaying = true
-        }
-    })
+    // $('#video').click(function() {
+    //     if (isPlaying) {
+    //         $('#video-background').animate({volume: 0})
+    //         $('.hide').fadeIn('slow')
+    //         isPlaying = false
+    //     } else {
+    //         $('#video-background').animate({volume: 1})
+    //         $('.hide').fadeOut('slow')
+    //         isPlaying = true
+    //     }
+    // })
 
-    $(document).scroll(function() {
+    $(document).scroll(function () {
         let nav = $('#nav-container')
-        if ($(document).scrollTop() > $('#video-background').height()) {
+        if ($(document).scrollTop() >= $('#video').height()) {
             console.log("passed nav")
-            $('.content-section').css('padding-top', '50px')
+            $('.content-section').css('padding-top', '70px')
             nav.css('position', 'fixed')
+            $('#top').slideDown('slow')
             navFixed = true
+
         } else {
-            let currentList = $('#nav li').map(function() {
+            let currentList = $('#nav li').map(function () {
                 return $.trim($(this).text())
             }).get()
             nav.css('position', 'relative')
-            $('.content-section').css('padding-top', '0')
-            $("#nav li:contains('Top')").html('<h3>' + list[currentList.indexOf('Top')] + '</h3>')
+            $('.content-section').css('padding-top', '20px')
+            $('#top').slideUp('slow')
             navFixed = false
         }
+
+        if ($(document).scrollTop() >= $('.content-section#support').position().top - 100) {
+            $('#footer').slideDown()
+        } else {
+            $('#footer').slideUp()
+            
+        }
     })
-    
-    $('#nav li').click(function() {
+
+    $('#nav li').click(function () {
         if (this.id !== 'zario') {
             let element = $('.content-section#' + this.id)
             let heightToScroll = $(element).offset().top
 
-            let currentList = $('#nav li').map(function() {
+            let currentList = $('#nav li').map(function () {
                 return $.trim($(this).text())
             }).get()
 
-            if ($(this).text() !== 'Top') {
+            let navItem = this
 
-                let navItem = this
-
-                if (navFixed) {
-                    $("html body").animate({scrollTop: heightToScroll - 20}, "slow", 'swing', function() {
-                        $(navItem).html('<h3>Top</h3>')
-                    })
-                } else {
-                    $("html body").animate({scrollTop: heightToScroll - 60}, "slow", 'swing', function() {
-                        $(navItem).html('<h3>Top</h3>')
-                    })
-                }
-                $("#nav li:contains('Top')").html('<h3>' + list[currentList.indexOf('Top')] + '</h3>')
-                
+            if (navFixed) {
+                $("html body").animate({ scrollTop: heightToScroll - 30 }, "slow", 'swing')
             } else {
-               
-                $("html body").animate({scrollTop: 0}, "slow")
-                $("#nav li:contains('Top')").html('<h3>' + list[currentList.indexOf('Top')] + '</h3>')  
+                $("html body").animate({ scrollTop: heightToScroll - 80 }, "slow", 'swing')
             }
+            $("#nav li:contains('Top')").html('<h3>' + list[currentList.indexOf('Top')] + '</h3>')
         }
     })
 
+    $('#top').click(function () {
+        $("html body").animate({ scrollTop: 0 }, "slow")
+    })
 })
-
